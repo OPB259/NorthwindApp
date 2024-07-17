d8fdf594bc81025be0853ea86c99b8a320d461e3
OPB259 <148637273+OPB259@users.noreply.github.com>
2024-07-17 20:23:16 +0200

    Add Northwind classes

---
+++ NorthwindApp/NorthwindApp/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Windows.Controls;
+
+public class NorthwindContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;");
+    }
+}
---
+++ NorthwindApp/NorthwindApp/Order.cs
@@ -0,0 +1,8 @@
+﻿public class Order
+{
+    public int OrderID { get; set; }
+    public string CustomerID { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string ShipName { get; set; }
+    public string ShipAddress { get; set; }
+}
